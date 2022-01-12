e40c5d0f37d81c8d94c2d0ba44ac56bb54228c72
hoangdv2429 <hoangdv2429@gmail.com>
2022-01-12 17:32:01 +0700

    why

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,13 +2,17 @@ require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString()
+
+const projectID = "c8c863096b0b4d63aab833f754606c1d"
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -17,5 +21,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: 'https://polygon-mumbai.infura.io/v3/${projectID}',
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: 'https://polygon-mumbai.infura.io/v3/${projectID}',
+      accounts: [privateKey]
+    }
+
+  },
   solidity: "0.8.4",
 };
+
